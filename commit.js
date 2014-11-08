e13023ce11458117a44daa5f0fbcfcf0ed7928ee
Craig Barraclough <craig@barraclough.mobi>
2014-11-08 13:32:44 +1100

    Correcting style

--- commit.js
+++ commit.js
@@ -19,4 +19,4 @@ var commit_messages = [
   "AAAAAAAAAAARGH!",
   "Fixed a typo",
   "Updated the readme",
-  "Fixing a typo added in the last readme up
+  "Fixing a typo added in the last readme upd
