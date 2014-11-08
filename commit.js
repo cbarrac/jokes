0ece733dd6e126165c2364f7c03bc6499defad21
Craig Barraclough <craig@barraclough.mobi>
2014-11-08 23:51:08 +1100

    Nearly got it

--- commit.js
+++ commit.js
@@ -19,4 +19,4 @@ var commit_messages = [
   "AAAAAAAAAAARGH!",
   "Fixed a typo",
   "Updated the readme",
-  "Fixing a typo added in the last readme update"
+  "Fixing a typo added in the last readme update",
