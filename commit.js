c48edd2b035a739a7d9bb496007378f37eaa021b
Craig Barraclough <craig@barraclough.mobi>
2014-11-15 08:52:00 +1100

    Who you calling a fool?

--- commit.js
+++ commit.js
@@ -22,4 +22,4 @@ var commit_messages = [
   "Fixing a typo added in the last readme update",
   "GET A HAIRCUT!",
   "Updated the readme again",
-  "Found an issue with the letter 'a', so
+  "Found an issue with the letter 'a', so 
