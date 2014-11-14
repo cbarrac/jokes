ff29e133a0cb8c52a1cac66e5346ca113e34314d
Craig Barraclough <craig@barraclough.mobi>
2014-11-15 04:01:17 +1100

    Ooops!

--- commit.js
+++ commit.js
@@ -22,4 +22,4 @@ var commit_messages = [
   "Fixing a typo added in the last readme update",
   "GET A HAIRCUT!",
   "Updated the readme again",
-  "Found an issue with the letter 'a', 
+  "Found an issue with the letter 'a', s
