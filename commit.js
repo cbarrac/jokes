156865cea9b0df899f0bf14be3035481919bec0b
Craig Barraclough <craig@barraclough.mobi>
2015-02-09 03:02:46 +1100

    Nearly got it

--- commit.js
+++ commit.js
@@ -60,4 +60,4 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
   var inFile = process.argv[ 2 ]
   var outFile = process.argv[ 3 ]
   if (inFile == outFile) {
-    console.error("Aborted: infile and outfile
+    console.error("Aborted: infile and outfile 
