1c4d6e9482bb2d825584452b64f8bb0519748f80
Craig Barraclough <craig@barraclough.mobi>
2015-03-20 05:42:02 +1100

    Updated the readme again

--- commit.js
+++ commit.js
@@ -70,4 +70,4 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
   fs.readFile(inFile, function(err,data) {
     var length = data.length
     var commit_count = Math.ceil(length / step)
-    var commit_time = co
+    var commit_time = com
