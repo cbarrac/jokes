cae8936e8d5d59f20c9575c1671a02d226852956
Craig Barraclough <craig@barraclough.mobi>
2015-05-29 18:38:42 +1000

    Five bullets or six?

--- commit.js
+++ commit.js
@@ -86,4 +86,4 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
       child_process.execFileSync('git', ['add', outFile])
     } catch (e) {
       console.error("Couldn't add %s to git: %s", outFile, e)
-    
+    }
