2aad66ef22b0787c0fa58a8df96c7ec452b49341
Craig Barraclough <craig@barraclough.mobi>
2015-06-18 12:17:04 +1000

    You feeling lucky punk?

--- commit.js
+++ commit.js
@@ -94,4 +94,4 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
       faketime(t_offset);
       timeout = 20;
     } else {
-      timeout = randomIntFromInterval(0, max_s
+      timeout = randomIntFromInterval(0, max_sl
