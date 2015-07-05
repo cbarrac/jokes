3ab6a5b2e2ff3c187c91ccef9a1456815b61def6
Craig Barraclough <craig@barraclough.mobi>
2015-07-05 21:05:37 +1000

    Did I do that?

--- commit.js
+++ commit.js
@@ -100,4 +100,4 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
   })
 } else {
   console.info("Usage:")
-  console.info("node commit.js infile outfile [max_time_in_seconds] [s
+  console.info("node commit.js infile outfile [max_time_in_seconds] [st
