c1b8de1745fbd2b6777e6cd78bcc2caffa4795bc
Craig Barraclough <craig@barraclough.mobi>
2015-10-15 16:17:00 +1100

    Reverting changes

--- commit.js
+++ commit.js
@@ -136,4 +136,4 @@ function commit(t_offset, outFD, outFile, data, counter, length)
   } else {
     var args = ['push']
     child_process.execFileSync('git', args)
-    console.info("Finished
+    console.info("Finished"
