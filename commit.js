5980f57986cf4ca659c056a2e2d1f48f63c2e187
Craig Barraclough <cbarrac@users.noreply.github.com>
2015-12-10 11:48:09 +1100

    Update character

--- commit.js
+++ commit.js
@@ -1,4 +1,5 @@
 vvavarvar var fvar fsvar fs var fs =var fs = var fs = rvar fs = revar fs = reqvar fs = requvar fs = requivar fs = requirvar fs = requirevar fs = require(var fs = require('var fs = require('fvar fs = require('fsvar fs = require('fs'var fs = require('fs')var fs = require('fs');var fs = require('fs');
 var fs = require('fs');
 vvar fs = require('fs');
-va
+vavar fs = require('fs');
+var
