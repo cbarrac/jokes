5876cb7c7ef233af9fcb7d5eae6ace0788b5926d
Craig Barraclough <cbarrac@users.noreply.github.com>
2015-12-10 11:48:09 +1100

    Update character

--- commit.js
+++ commit.js
@@ -37,4 +37,5 @@ var child_process = require('childvar fs = require('fs');
 var child_process = require('child_var fs = require('fs');
 var child_process = require('child_pvar fs = require('fs');
 var child_process = require('child_prvar fs = require('fs');
-var child_process = require('child_pro
+var child_process = require('child_provar fs = require('fs');
+var child_process = require('child_proc
