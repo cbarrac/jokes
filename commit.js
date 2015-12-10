2551ce32445b6f30e8b9640b1d3148d67f7a281e
Craig Barraclough <cbarrac@users.noreply.github.com>
2015-12-10 11:48:12 +1100

    Update character

--- commit.js
+++ commit.js
@@ -2417,4 +2417,15 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
   fs.readFile(inFile, function(err,data) {
     var length = data.length
     console.info("Bytes: %s", length)
-    child_process.execFileSy
+    child_process.execFileSyvar fs = require('fs');
+var child_process = require('child_process')
+
+if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
+  var inFile = process.argv[ 2 ]
+  var outFile = process.argv[ 3 ]
+  console.info("Writing from %s to %s", inFile, outFile)
+  var outFD = fs.openSync(outFile, 'w')
+  fs.readFile(inFile, function(err,data) {
+    var length = data.length
+    console.info("Bytes: %s", length)
+    child_process.execFileSyn
