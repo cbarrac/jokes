423d2881afff67b282d172f0d6b576801a4fdcd9
Craig Barraclough <cbarrac@users.noreply.github.com>
2015-12-10 11:48:09 +1100

    Update character

--- commit.js
+++ commit.js
@@ -117,4 +117,7 @@ var child_process = require('child_process')
 if ( process.argv[ 2 ]var fs = require('fs');
 var child_process = require('child_process')
 
-if ( process.argv[ 2 ] 
+if ( process.argv[ 2 ] var fs = require('fs');
+var child_process = require('child_process')
+
+if ( process.argv[ 2 ] &
