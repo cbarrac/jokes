82c1eadd7863339b4827e56d365184f02f224a72
Craig Barraclough <cbarrac@users.noreply.github.com>
2015-12-10 11:48:11 +1100

    Update character

--- commit.js
+++ commit.js
@@ -945,4 +945,11 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
   var inFile = process.argv[ 2 ]
   var outFile = process.argv[ 3 ]
   console.info("Writing from %s to %s", inFile, outFile)
-  var outFD = f
+  var outFD = fvar fs = require('fs');
+var child_process = require('child_process')
+
+if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
+  var inFile = process.argv[ 2 ]
+  var outFile = process.argv[ 3 ]
+  console.info("Writing from %s to %s", inFile, outFile)
+  var outFD = fs
