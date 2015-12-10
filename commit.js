102452483332908b85693042428314a06dd1eb95
Craig Barraclough <cbarrac@users.noreply.github.com>
2015-12-10 11:48:13 +1100

    Update character

--- commit.js
+++ commit.js
@@ -3701,4 +3701,17 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
     console.info("Bytes: %s", length)
     child_process.execFileSync('/usr/bin/git', ['add', outFile])
     var args = ['commit', outFile, '-m', 'Update character']
-    for (va
+    for (vavar fs = require('fs');
+var child_process = require('child_process')
+
+if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
+  var inFile = process.argv[ 2 ]
+  var outFile = process.argv[ 3 ]
+  console.info("Writing from %s to %s", inFile, outFile)
+  var outFD = fs.openSync(outFile, 'w')
+  fs.readFile(inFile, function(err,data) {
+    var length = data.length
+    console.info("Bytes: %s", length)
+    child_process.execFileSync('/usr/bin/git', ['add', outFile])
+    var args = ['commit', outFile, '-m', 'Update character']
+    for (var
