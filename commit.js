5b5ed2002843f8a6ff7085bab4d23bb0fdb09e0b
Craig Barraclough <cbarrac@users.noreply.github.com>
2015-12-15 13:04:37 +1100

    Update character

--- commit.js
+++ commit.js
@@ -27,4 +27,4 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
 }
 
 function sleep(seconds) {
-  var endTime = new Date().getTime() + (seconds * 1000)
+  var endTime = new Date().getTime() + (seconds * 1000);
