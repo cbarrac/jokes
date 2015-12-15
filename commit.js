d8ff38d1e33187469b1d002c9e056e345bb10d3b
Craig Barraclough <cbarrac@users.noreply.github.com>
2015-12-15 11:56:12 +1100

    Update character

--- commit.js
+++ commit.js
@@ -5,4 +5,4 @@ var max_sleep = 300
 if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
   var inFile = process.argv[ 2 ]
   var outFile = process.argv[ 3 ]
-  if (process.argv [ 4 ]) max_sleep =
+  if (process.argv [ 4 ]) max_sleep = 
