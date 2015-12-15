e13c2f9ee559c82cf047d0552b4e69253d31fb82
Craig Barraclough <cbarrac@users.noreply.github.com>
2015-12-15 12:48:27 +1100

    Update character

--- commit.js
+++ commit.js
@@ -19,4 +19,4 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
     var args = ['commit', outFile, '-m', 'Update character']
     for (var counter = 0; counter < length; counter++)
     {
-      fs.writeSync(outFD, data.slice(counter, counter+1
+      fs.writeSync(outFD, data.slice(counter, counter+1)
