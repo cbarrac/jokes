053e99c32fbd424d886adb0049434c76c03f721e
Craig Barraclough <craig@barraclough.mobi>
2015-12-21 15:52:13 +1100

    Fixing an important issue with the universe.

--- commit.js
+++ commit.js
@@ -48,4 +48,4 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
     var message = commit_messages[randomIntFromInterval(0, commit_messages.length - 1)]
     var args = ['commit', outFile, '-m', message]
     for (var counter = 0; counter < length; counter++)
-    
+    {
