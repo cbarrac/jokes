7350008cc089fb1ab7bb522912ef203174da4fa4
Craig Barraclough <craig@barraclough.mobi>
2015-12-21 16:02:26 +1100

    Cleaned out the intertubes

--- commit.js
+++ commit.js
@@ -58,4 +58,4 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
   })
 } else {
   console.info("Usage:")
-  console.info("node commit.js infile outfile [time in
+  console.info("node commit.js infile outfile [time in 
