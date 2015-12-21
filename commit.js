96a23ae784e23a4891a4f7f7543bbd8edda78a16
Craig Barraclough <craig@barraclough.mobi>
2015-12-21 16:04:39 +1100

    Found an issue with the letter 'a', so I fixed it.

--- commit.js
+++ commit.js
@@ -62,4 +62,4 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
 }
 
 function sleep(seconds) {
-  var endTime = new Date().getTime() + (seconds * 1
+  var endTime = new Date().getTime() + (seconds * 10
