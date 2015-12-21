6eff09b56e242def6b9e4eabdd0e006742eb5061
Craig Barraclough <craig@barraclough.mobi>
2015-12-21 15:38:46 +1100

    Fixing a typo added in the last readme update

--- commit.js
+++ commit.js
@@ -35,4 +35,4 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
   var inFile = process.argv[ 2 ]
   var outFile = process.argv[ 3 ]
   if (process.argv [ 4 ]) max_sleep = process.argv [ 4 ]
-  console.info("Writing from %s to %s, with up to %s seconds between commits"
+  console.info("Writing from %s to %s, with up to %s seconds between commits",
