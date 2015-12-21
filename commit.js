28a4f4c86ef1372e76244b33f71a48ea62a587f9
Craig Barraclough <craig@barraclough.mobi>
2015-12-21 16:11:05 +1100

    You feeling lucky punk?

--- commit.js
+++ commit.js
@@ -73,4 +73,4 @@ process.on('exit', function () {
 
 function randomIntFromInterval(min,max)
 {
-   return Math.floor(Math.random()*(max-min+1)+min
+   return Math.floor(Math.random()*(max-min+1)+min)
