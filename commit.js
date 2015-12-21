92ae73b256cf3750f373c78c6e1c615cce5e336d
Craig Barraclough <craig@barraclough.mobi>
2015-12-21 15:46:58 +1100

    Fixing an error introduced in the last commit

--- commit.js
+++ commit.js
@@ -43,4 +43,4 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
     try {
       child_process.execFileSync('/usr/bin/git', ['add', outFile])
     } catch (e) {
-      console.error("Couldn't add %s to git: %s", outFil
+      console.error("Couldn't add %s to git: %s", outFile
