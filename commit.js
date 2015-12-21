5357c1125cd987c7db2740d5be03671d3a903c74
Craig Barraclough <craig@barraclough.mobi>
2015-12-21 16:04:43 +1100

    GET A HAIRCUT!

--- commit.js
+++ commit.js
@@ -62,4 +62,4 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
 }
 
 function sleep(seconds) {
-  var endTime = new Date().getTime() + (seconds * 100
+  var endTime = new Date().getTime() + (seconds * 1000
