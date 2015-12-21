34bc1ffb457bf77099ad98e16b69bbcf33c8b15b
Craig Barraclough <craig@barraclough.mobi>
2015-12-21 15:15:51 +1100

    Incompatible dimensions found, Cthuhlu Error 666

--- commit.js
+++ commit.js
@@ -6,4 +6,4 @@ var max_sleep = 300
 var commit_messages = [
   "Fixing an important issue with the universe.",
   "Someone poisoned the waterhole!",
-  "You feeling lucky pun
+  "You feeling lucky punk
