bb3f82339d71fdd953503c23ed0bcdf967d8728d
Craig Barraclough <craig@barraclough.mobi>
2015-12-21 15:14:09 +1100

    Fixed memory parsing error #1337

--- commit.js
+++ commit.js
@@ -4,4 +4,4 @@ var max_sleep = 300
 //Added a comment that achieves no real goal.
 
 var commit_messages = [
-  "Fixing an important issue with the univers
+  "Fixing an important issue with the universe
