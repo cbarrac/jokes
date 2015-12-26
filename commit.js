5037f50e51cfa97416c9849212c3b4e50f080fde
Craig Barraclough <craig@barraclough.mobi>
2015-12-27 09:25:47 +1100

    Grammar fix

--- commit.js
+++ commit.js
@@ -2,4 +2,5 @@ var child_process = require('child_process')
 var fs = require('fs');
 var max_sleep = 300
 var step = 10
-//Added a comment that achieves no re
+//Added a comment that achieves no real goal.
+//Non-block
