6f79349bfdfe588553bf8c62e5a7d67c1f8d78ea
Craig Barraclough <craig@barraclough.mobi>
2015-12-30 08:05:47 +1100

    Fixing punctuation

--- commit.js
+++ commit.js
@@ -41,4 +41,5 @@ var commit_messages = [
   "Ooops!",
   "Did I do that?",
   "Commit all the required changes",
-  "Really commit all the required change
+  "Really commit all the required changes",
+  "Should work n
