3a1cc38dc310a2d4ac311d9b93ddb68d387e7de9
Craig Barraclough <craig@barraclough.mobi>
2015-12-31 19:42:20 +1100

    Re-updating the readme.

--- commit.js
+++ commit.js
@@ -61,4 +61,5 @@ var commit_messages = [
 if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
   var inFile = process.argv[ 2 ]
   var outFile = process.argv[ 3 ]
-  if (inF
+  if (inFile == outFile) {
+  
