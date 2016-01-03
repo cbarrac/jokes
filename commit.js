0e7804faeb90a4c9515d4b77fbafff0fec2e459e
Craig Barraclough <craig@barraclough.mobi>
2016-01-04 03:34:27 +1100

    Back out the last commit

--- commit.js
+++ commit.js
@@ -82,4 +82,4 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
     if (process.env.FAKETIME) {
       // 10% buffer, to reduce the likelihood of going in to the future
       t_offset = commit_time * -1.1;
-      
+      faketime(t_offset);
