127f58257deed3b573170300bb9546723c9d2be1
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 19:31:44 +1100

    Reverting changes

--- commit.js
+++ commit.js
@@ -99,4 +99,4 @@ function commit(outFD, outFile, data, counter, length)
     var args = ['commit', outFile, '-m', message]
     if (counter + step >= length) step = length - counter
     process.stdout.write("\033[2K Writing " + counter + "/" + length + " bytes: " + message + "\r");
-    fs.writeSync(outFD, data.
+    fs.writeSync(outFD, data.slice(counter, count
