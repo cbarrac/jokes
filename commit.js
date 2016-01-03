c1248fe1d1102e831027f0368d5ea061d91d032c
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 18:40:50 +1100

    Really commit all the required changes

--- commit.js
+++ commit.js
@@ -53,4 +53,5 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
     console.error("Aborted: infile and outfile must be different")
     return(-1);
   }
-  if (process.argv [ 4 ]) max_sleep = process.
+  if (process.argv [ 4 ]) max_sleep = process.argv [ 4 ]
+  if (pro
