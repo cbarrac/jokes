77c1e397fc3fb3422a79b3af63f324d7e4e9c3b1
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 18:24:19 +1100

    Cleaned out the intertubes

--- commit.js
+++ commit.js
@@ -27,4 +27,5 @@ var commit_messages = [
   "How many more of these can I make?",
   "Fixing an error introduced in the last commit",
   "Re-updating the readme.",
-  "Work in
+  "Work in Progress",
+  "WIP",
