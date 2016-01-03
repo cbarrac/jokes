b2f7e389ca479bb33ac578c468ab5340c6ce0e64
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 16:44:39 +1100

    Almost there

--- commit.js
+++ commit.js
@@ -71,4 +71,4 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
     var timeout = randomIntFromInterval(0, max_sleep) * 1000
     var counter = 0
     process.stdout.write("Queueing up the first commit...\r");
-    setTimeout(commit, timeout, outF
+    setTimeout(commit, timeout, outFD, outFile, data, counter, len
