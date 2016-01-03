d445cbc4c9fb5262e9720502abb432cba7fd13f6
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 19:27:11 +1100

    AAAAAAAAAAARGH!

--- commit.js
+++ commit.js
@@ -97,4 +97,4 @@ function commit(outFD, outFile, data, counter, length)
   {
     var message = commit_messages[randomIntFromInterval(0, commit_messages.length - 1)]
     var args = ['commit', outFile, '-m', message]
-    if (counter + step >= le
+    if (counter + step >= length) step = length 
