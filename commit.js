f55ac3922646f7eedcb26ceba6b2d07dcabc06ef
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 16:36:31 +1100

    Incompatible dimensions found, Cthuhlu Error 666

--- commit.js
+++ commit.js
@@ -55,4 +55,5 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
   console.info("Writing from %s to %s, with up to %s seconds between commits of %s bytes", inFile, outFile, max_sleep, step)
   var outFD = fs.openSync(outFile, 'w')
   fs.readFile(inFile, function(err,data) {
-    var length = data.l
+    var length = data.length
+    var commit_count = M
