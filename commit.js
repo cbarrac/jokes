b693fd0fb0ee499ba9f6fe26a7ebd64d92979730
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 15:42:45 +1100

    Almost there

--- commit.js
+++ commit.js
@@ -74,4 +74,5 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
 }
 
 function sleep(seconds) {
-  var endTime = new Date().getTime() + (seconds * 100
+  var endTime = new Date().getTime() + (seconds * 1000);
+  while (new Date().getTime() <= endTime) {;}
