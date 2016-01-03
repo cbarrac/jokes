a64890c147a9a6de35adc889c3a55ac9410e46a3
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 16:53:25 +1100

    Fixed memory parsing error #1337

--- commit.js
+++ commit.js
@@ -96,4 +96,5 @@ function commit(outFD, outFile, data, counter, length)
     var args = ['commit', outFile, '-m', message]
     if (counter + step >= length) step = length - counter
     process.stdout.write("\033[2K Writing " + counter + "/" + length + " bytes: " + message + "\r");
-    fs.writeSync(outFD, data.slice(counter,
+    fs.writeSync(outFD, data.slice(counter, counter+step), 0, step)
+    c
