f1302a3ec2da9efa4e852bc2adc43b5d8c568123
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 15:45:33 +1100

    Found an issue with the letter 'a', so I fixed it.

--- commit.js
+++ commit.js
@@ -88,4 +88,5 @@ function commit(outFD, outFile, data, counter, length)
   if (counter < length)
   {
     var message = commit_messages[randomIntFromInterval(0, commit_messages.length - 1)]
-    var args = ['commit', 
+    var args = ['commit', outFile, '-m', message]
+    if (counter + step >= 
