31f8e078ed077efcba516cd668f4243fd6281fd0
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 16:22:35 +1100

    Did I do that?

--- commit.js
+++ commit.js
@@ -22,4 +22,5 @@ var commit_messages = [
   "GET A HAIRCUT!",
   "Updated the readme again",
   "Found an issue with the letter 'a', so I fixed it.",
-  "Incompatible dimensions found,
+  "Incompatible dimensions found, Cthuhlu Error 666",
+  "Who yo
