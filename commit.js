8d3ba4ed913dae71de1aa491f285a9e6f79e36de
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 16:49:43 +1100

    Work in Progress

--- commit.js
+++ commit.js
@@ -92,4 +92,4 @@ function commit(outFD, outFile, data, counter, length)
 {
   if (counter < length)
   {
-    var message = commit_messages[random
+    var message = commit_messages[randomIntFromInterval(0, commit_mess
