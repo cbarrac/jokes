9ea0e8d167494378243c8c64191044719768e188
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 19:19:39 +1100

    Work in Progress

--- commit.js
+++ commit.js
@@ -88,4 +88,7 @@ function sleep(seconds) {
 
 function randomIntFromInterval(min,max)
 {
-   return Math.floor(Math.random()*(max-min+1)+
+   return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+function co
