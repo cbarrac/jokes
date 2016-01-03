4c966edd45ec88454f9942f7ef880972799879c6
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 18:38:18 +1100

    Five bullets or six?

--- commit.js
+++ commit.js
@@ -50,4 +50,5 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
   var inFile = process.argv[ 2 ]
   var outFile = process.argv[ 3 ]
   if (inFile == outFile) {
-    console.error("Aborted: infile and outfile must b
+    console.error("Aborted: infile and outfile must be different")
+    re
