e321c6fdf59830b8393da96bfbee5d1f915c14e5
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 15:36:25 +1100

    Fixed memory parsing error #1337

--- commit.js
+++ commit.js
@@ -55,4 +55,6 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
   var outFD = fs.openSync(outFile, 'w')
   fs.readFile(inFile, function(err,data) {
     var length = data.length
-   
+    console.info("Bytes: %s", length)
+    try {
+     
