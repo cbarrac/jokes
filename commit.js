f6f4f0a3d4b14e7ed4ee1d90dca2338ca3e7521a
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 19:26:04 +1100

    Grammar fix

--- commit.js
+++ commit.js
@@ -96,4 +96,5 @@ function commit(outFD, outFile, data, counter, length)
   if (counter < length)
   {
     var message = commit_messages[randomIntFromInterval(0, commit_messages.length - 1)]
-    var args = ['commit', outFile, '-m
+    var args = ['commit', outFile, '-m', message]
+    if (
