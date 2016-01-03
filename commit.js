4f8d5927286102a5fed7dbfc5debf8ddce5edb43
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 16:50:51 +1100

    Not quite working

--- commit.js
+++ commit.js
@@ -93,4 +93,5 @@ function commit(outFD, outFile, data, counter, length)
   if (counter < length)
   {
     var message = commit_messages[randomIntFromInterval(0, commit_messages.length - 1)]
-    var args = ['commit', outFile, '-m', m
+    var args = ['commit', outFile, '-m', message]
+    if (counter + step
