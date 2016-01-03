10d0a7505909c32e3d2213826fd113e3b03e4cb7
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 18:50:42 +1100

    Not quite working

--- commit.js
+++ commit.js
@@ -58,4 +58,5 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
   console.info("Writing from %s to %s, with up to %s seconds between commits of %s bytes", inFile, outFile, max_sleep, step)
   var outFD = fs.openSync(outFile, 'w')
   fs.readFile(inFile, function(err,data) {
-    var length = da
+    var length = data.length
+    var co
