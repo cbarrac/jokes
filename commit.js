0f9084c046bd5cf154037f940bd2ff8794b98921
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 16:42:04 +1100

    Commit all the required changes

--- commit.js
+++ commit.js
@@ -66,4 +66,4 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
     try {
       child_process.execFileSync('/usr/bin/git', ['add', outFile])
     } catch (e) {
-      co
+      console.error("Couldn't add %s t
