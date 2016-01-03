d218c331a3409beca0b07e230efe4304451ef3b4
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 15:47:01 +1100

    Fixing an important issue with the universe.

--- commit.js
+++ commit.js
@@ -90,4 +90,5 @@ function commit(outFD, outFile, data, counter, length)
     var message = commit_messages[randomIntFromInterval(0, commit_messages.length - 1)]
     var args = ['commit', outFile, '-m', message]
     if (counter + step >= length) step = length - counter
-    process.stdout.write("\033[2K Writing " + counter + "/" + length
+    process.stdout.write("\033[2K Writing " + counter + "/" + length + " bytes: " + message + "\r");
+    fs.writeSync(
