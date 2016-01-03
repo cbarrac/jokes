89ddc1732f0a6897e7d966bf8dfb4f71c314a679
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 19:36:39 +1100

    Reverting changes

--- commit.js
+++ commit.js
@@ -103,4 +103,4 @@ function commit(outFD, outFile, data, counter, length)
     child_process.execFileSync('/usr/bin/git', args)
     var timeout = randomIntFromInterval(0, max_sleep) * 1000
     counter = counter + step
-    setTimeout(commit, timeout, outFD,
+    setTimeout(commit, timeout, outFD, outFile, data, coun
