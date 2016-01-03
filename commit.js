86481aac52bbfe9269aec8f754eec63ae6eead1f
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 16:50:30 +1100

    Work in Progress

--- commit.js
+++ commit.js
@@ -93,4 +93,4 @@ function commit(outFD, outFile, data, counter, length)
   if (counter < length)
   {
     var message = commit_messages[randomIntFromInterval(0, commit_messages.length - 1)]
-    var args
+    var args = ['commit', outFile, '-m', m
