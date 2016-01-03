ff69ceb0de3f5dad1b13bd7a9adba10b2f2b6d9d
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 15:50:23 +1100

    Found an issue with the letter 'a', so I fixed it.

--- commit.js
+++ commit.js
@@ -98,3 +98,4 @@ function commit(outFD, outFile, data, counter, length)
     setTimeout(commit, timeout, outFD, outFile, data, counter, length);
   } else {
     var args = ['push']
+    child_process.execFileSync('/usr/bin/git', ar
