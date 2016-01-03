7445bcce7fd614491bbc58fb221a72a739096054
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 19:13:08 +1100

    Someone poisoned the waterhole!

--- commit.js
+++ commit.js
@@ -78,4 +78,4 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
   })
 } else {
   console.info("Usage:")
-  console.info("node commit.js infile out
+  console.info("node commit.js infile outfile [max_time_in_se
