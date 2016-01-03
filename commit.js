16cf3f794a0ebbe0c46d7ce742295eb93c170ceb
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 15:37:27 +1100

    AAAAAAAAAAARGH!

--- commit.js
+++ commit.js
@@ -61,4 +61,4 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
     } catch (e) {
       console.error("Couldn't add %s to git: %s", outFile, e)
     }
-  
+    var message = commit_messages[randomIntFromInter
