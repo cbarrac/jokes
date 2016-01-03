88ba4c0b3ddb00394f87d0e0aaef7f81c26b56cd
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 16:36:56 +1100

    Grammar fix

--- commit.js
+++ commit.js
@@ -56,4 +56,5 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
   var outFD = fs.openSync(outFile, 'w')
   fs.readFile(inFile, function(err,data) {
     var length = data.length
-    var commit_count = M
+    var commit_count = Math.ceil(length / step)
+    va
