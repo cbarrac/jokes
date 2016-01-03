7c3afebaccd2bc1e5a65aa72fb2f2ebcf8e33730
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 16:55:58 +1100

    Updated the readme again

--- commit.js
+++ commit.js
@@ -102,4 +102,5 @@ function commit(outFD, outFile, data, counter, length)
     counter = counter + step
     setTimeout(commit, timeout, outFD, outFile, data, counter, length);
   } else {
-    var args = ['pu
+    var args = ['push']
+    child_process.execFil
