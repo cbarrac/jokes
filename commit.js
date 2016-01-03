e8621544f84eb1979cd66d543f04d9cee21651fb
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 18:52:53 +1100

    Fixed a typo

--- commit.js
+++ commit.js
@@ -61,4 +61,4 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
     var length = data.length
     var commit_count = Math.ceil(length / step)
     var commit_time = commit_count * max_sleep / 2
-    var com
+    var commit_time_hour = Math
