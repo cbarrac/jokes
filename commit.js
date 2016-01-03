a32152a884a71e766ecc67d42dc81b89d72fec3f
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 15:41:22 +1100

    Almost there

--- commit.js
+++ commit.js
@@ -70,4 +70,8 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
   })
 } else {
   console.info("Usage:")
-  console.info("node commit.js infile outfile [max_time_in_
+  console.info("node commit.js infile outfile [max_time_in_seconds] [step]")
+}
+
+function sleep(seconds) {
+  v
