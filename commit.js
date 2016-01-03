310df128af8937afc1d6074f4b955dee9559bdf5
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 15:49:13 +1100

    Commit all the required changes

--- commit.js
+++ commit.js
@@ -92,4 +92,5 @@ function commit(outFD, outFile, data, counter, length)
     if (counter + step >= length) step = length - counter
     process.stdout.write("\033[2K Writing " + counter + "/" + length + " bytes: " + message + "\r");
     fs.writeSync(outFD, data.slice(counter, counter+step), 0, step)
-    child_process.execFileSync('/usr/bin/git', ar
+    child_process.execFileSync('/usr/bin/git', args)
+    var timeout = randomIntFromInterval(0, max
