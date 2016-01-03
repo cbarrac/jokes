b9144a9a83f2e8168a53435a89068ebb98c53ae5
Craig Barraclough <craig@barraclough.mobi>
2016-01-03 19:15:12 +1100

    Incompatible dimensions found, Cthuhlu Error 666

--- commit.js
+++ commit.js
@@ -82,4 +82,5 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
 }
 
 function sleep(seconds) {
-  var endTime = new Date().getTime() + (seconds * 1000)
+  var endTime = new Date().getTime() + (seconds * 1000);
+  while (new Date(
