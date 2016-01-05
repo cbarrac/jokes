a8cef263cacf349004478569babca6ed76015fec
Craig Barraclough <craig@barraclough.mobi>
2016-01-05 18:02:06 +1100

    Work in Progress

--- commit.js
+++ commit.js
@@ -50,4 +50,5 @@ var commit_messages = [
 
 if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
   var inFile = process.argv[ 2 ]
-  var out
+  var outFile = process.argv[ 3 ]
+  if (inFile == outFile) 
