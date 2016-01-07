60aa4f18fde288921894593add181e37e7e3ed3e
Craig Barraclough <craig@barraclough.mobi>
2016-01-07 23:38:05 +1100

    Re-updating the readme.

--- commit.js
+++ commit.js
@@ -85,4 +85,7 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
 
 function sleep(seconds) {
   var endTime = new Date().getTime() + (seconds * 1000);
-  while (new Date().get
+  while (new Date().getTime() <= endTime) {;}
+}
+
+function randomIntFromIn
