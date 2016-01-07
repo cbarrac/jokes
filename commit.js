e832121dc1b1a0392651e3e25b78fa71960ef69f
Craig Barraclough <craig@barraclough.mobi>
2016-01-07 23:41:47 +1100

    One more time...

--- commit.js
+++ commit.js
@@ -99,4 +99,5 @@ function commit(outFD, outFile, data, counter, length)
   {
     var message = commit_messages[randomIntFromInterval(0, commit_messages.length - 1)]
     var args = ['commit', outFile, '-m', message]
-    if (counter + step >= length) step = length - cou
+    if (counter + step >= length) step = length - counter
+    process.stdout.write("\033[2K Writing " +
