dd49351628718c56d341763b23dff53a07eee360
Craig Barraclough <craig@barraclough.mobi>
2016-01-07 23:35:03 +1100

    Re-updating the readme.

--- commit.js
+++ commit.js
@@ -75,4 +75,4 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
     }
     var timeout = randomIntFromInterval(0, max_sleep) * 1000
     var counter = 0
-    proces
+    process.stdout.write("Queueing up the first commit...\r"
