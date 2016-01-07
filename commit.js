86ec0f7d04857b6014236447fb0292fe852a7e90
Craig Barraclough <craig@barraclough.mobi>
2016-01-07 23:46:21 +1100

    Should work now

--- commit.js
+++ commit.js
@@ -108,4 +108,5 @@ function commit(outFD, outFile, data, counter, length)
     setTimeout(commit, timeout, outFD, outFile, data, counter, length);
   } else {
     var args = ['push']
-    child_process.execFile
+    child_process.execFileSync('/usr/bin/git', args)
+    console.info("Finis
