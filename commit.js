0d17a9480c2b194e47dc7fffa4c0a8be9ba1d744
Craig Barraclough <craig@barraclough.mobi>
2016-01-08 14:45:20 +1100

    Incompatible dimensions found, Cthuhlu Error 666

--- commit.js
+++ commit.js
@@ -105,4 +105,5 @@ function commit(outFD, outFile, data, counter, length)
   if (counter < length)
   {
     var message = commit_messages[randomIntFromInterval(0, commit_messages.length - 1)]
-    var args = ['commit', 
+    var args = ['commit', outFile, '-m', message]
+    if (counter 
