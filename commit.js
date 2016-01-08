577c3449ff7f0a5ea432ed22f8123accdbca3763
Craig Barraclough <craig@barraclough.mobi>
2016-01-08 14:40:09 +1100

    Fast-forwarding changes

--- commit.js
+++ commit.js
@@ -104,4 +104,4 @@ function commit(outFD, outFile, data, counter, length)
 {
   if (counter < length)
   {
-    var message = commit_messages[
+    var message = commit_messages[randomIntFromInterval(0, commit_messages
