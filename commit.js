67b1e2fe77d0e198a0f04a75c8d9a37751d96589
Craig Barraclough <craig@barraclough.mobi>
2016-01-08 13:42:15 +1100

    Fast-forwarding changes

--- commit.js
+++ commit.js
@@ -70,4 +70,5 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
     var length = data.length
     var commit_count = Math.ceil(length / step)
     var commit_time = commit_count * max_sleep / 2
-    var commit_time
+    var commit_time_hour = Math.floor(commit_time / 3600)
+ 
