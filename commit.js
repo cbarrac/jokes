70041dca03acc4c2239ae453a004b4c127290fe4
Craig Barraclough <craig@barraclough.mobi>
2016-01-08 13:45:03 +1100

    Updated the readme again

--- commit.js
+++ commit.js
@@ -71,4 +71,4 @@ if ( process.argv[ 2 ] && process.argv[ 3 ] ) {
     var commit_count = Math.ceil(length / step)
     var commit_time = commit_count * max_sleep / 2
     var commit_time_hour = Math.floor(commit_time / 3600)
- 
+    var commit_time_rem = commit_time - (
