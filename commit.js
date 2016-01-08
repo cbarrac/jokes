5bacd351059d82b6b0ba026c30bea738724d3af2
Craig Barraclough <craig@barraclough.mobi>
2016-01-08 14:46:34 +1100

    Nearly got it

--- commit.js
+++ commit.js
@@ -106,4 +106,4 @@ function commit(outFD, outFile, data, counter, length)
   {
     var message = commit_messages[randomIntFromInterval(0, commit_messages.length - 1)]
     var args = ['commit', outFile, '-m', message]
-    if (counter 
+    if (counter + step >= length) step = length - counte
