e7b2236d6d803d8b31b085e7661e3248ec559aa2
Craig Barraclough <craig@barraclough.mobi>
2016-01-08 12:16:42 +1100

    Not quite working

--- commit.js
+++ commit.js
@@ -3,4 +3,6 @@ var child_process = require('child_process')
 var max_sleep = 300
 var step = 10
 //Added a comment that achieves no real goal.
-//Non-block
+//Non-blocking loop
+//Adjustable step
+//Status line
