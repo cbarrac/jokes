61a6673147cb7cfac2a7246e1c6fef5e5985199e
Craig Barraclough <craig@barraclough.mobi>
2016-01-08 12:14:05 +1100

    Fast-forwarding changes

--- commit.js
+++ commit.js
@@ -2,4 +2,5 @@ var fs = require('fs');
 var child_process = require('child_process')
 var max_sleep = 300
 var step = 10
-//Added a comment
+//Added a comment that achieves no real goal.
+//Non-block
