61ba08f3da6b0e4b54154bc1d2fadf2f0525df1f
Craig Barraclough <craig@barraclough.mobi>
2016-01-09 16:26:34 +1100

    Well that seemed pretty easy

--- commit.js
+++ commit.js
@@ -141,4 +141,4 @@ function commit(t_offset, outFD, outFile, data, counter, length)
     setTimeout(commit, timeout, t_offset, outFD, outFile, data, counter, length);
   } else {
     var args = ['push']
-    child_proc
+    child_process.execFileSync('gi
